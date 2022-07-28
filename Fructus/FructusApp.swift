eb79afb8b6e625111638075591221748db7ef725
alafty <mohamedelelafty@yahoo.com>
2022-07-28 23:18:30 +0200

    Initial Commit

---
+++ Fructus/FructusApp.swift
@@ -0,0 +1,17 @@
+//
+//  FructusApp.swift
+//  Fructus
+//
+//  Created by Mohamed El-Alafty on 28/07/2022.
+//
+
+import SwiftUI
+
+@main
+struct FructusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
